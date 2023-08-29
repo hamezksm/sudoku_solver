f67fdbead6fd0917d2fe109ae2defb79904f8e01
hamezksm <jamcav097@gmail.com>
2023-08-29 22:01:01 +0300

    Added the theme

---
+++ lib/screens/solver.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Solver extends StatefulWidget {
+  const Solver({super.key});
+
+  @override
+  State<Solver> createState() => _SolverState();
+}
+
+class _SolverState extends State<Solver> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
